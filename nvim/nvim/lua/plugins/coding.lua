60e94037e9aecd1b9b81c5b083bc4496d350885c
Slava Buzin <sbuzin@vbmaca.local>
2025-05-28 21:52:13 +0200

    nvim coding config

--- nvim/nvim/lua/plugins/coding.lua
+++ nvim/nvim/lua/plugins/coding.lua
@@ -12,11 +12,105 @@ return {
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
     opts = {
-      ensure_installed = { "vim", "vimdoc", "lua" },
-      auto_install = true,
+      ensure_installed = {
+        "vim",
+        "vimdoc",
+        "lua"
+      },
+      auto_install = false,
       highlight = { enable = true },
       incremental_selection = { enable = true },
       indent = { enable = true },
     },
+  },
+  -- LSP Configuration
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+
+      -- Define on_attach function (reused for all LSPs)
+        local on_attach = function(client, bufnr)
+
+          local opts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<leader>ds', vim.diagnostic.open_float, opts)
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        end
+
+        -- Get capabilities from nvim-cmp
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+        capabilities.workspace = capabilities.workspace or {}
+        capabilities.workspace.didChangeWatchedFiles = {
+          dynamicRegistration = true,
+        }
+
+      end,
+    },
+
+    -- Formatting with conform.nvim
+    {
+      "stevearc/conform.nvim",
+      config = function()
+        require("conform").setup({
+          formatters_by_ft = {
+          },
+          format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true, -- Use LSP formatting for unspecified filetypes or if primary fails
+          },
+        })
+      end,
+
+      event = { "BufWritePre" },
+      cmd = { "ConformInfo" },
+    },
+
+    -- Autocompletion
+    {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+      },
+      opts = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        return {
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          }),
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+          }),
+          window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered()
+          }
+        }
+      end,
+      config = function(_, opts)
+        require("luasnip.loaders.from_vscode").lazy_load() -- Load VSCode-style snippets
+        require("cmp").setup(opts)
+      end,
+    }
   }
-}
