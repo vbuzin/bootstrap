6f57c728d61c6a49501f202fb56ecb51963ee3ae
Slava Buzin <31203090+vbuzin@users.noreply.github.com>
2026-02-19 21:59:17 +0100

    fix fsharp in nvim

--- nvim/nvim/lua/plugins/lang/fsharp.lua
+++ nvim/nvim/lua/plugins/lang/fsharp.lua
@@ -1,55 +1,65 @@
 return {
-  -- Add F# filetype detection
-  {
-    "neovim/nvim-lspconfig",
-    ft = { "fsharp" },
-    init = function()
-      vim.filetype.add({
-        extension = { fs = "fsharp", fsx = "fsharp", fsi = "fsharp" },
-      })
-    end,
-    opts = {
-      servers = {
-        fsautocomplete = {
-          root_markers = { ".sln", ".fsproj", ".fsx" },
-        },
-      },
-    },
-  },
+	-- Add F# filetype detection
+	{
+		"neovim/nvim-lspconfig",
+		ft = { "fsharp" },
+		init = function()
+			vim.filetype.add({
+				extension = { fs = "fsharp", fsx = "fsharp", fsi = "fsharp" },
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "fsharp",
+				callback = function()
+					-- Match Fantomas default indentation (4 spaces)
+					vim.opt_local.expandtab = true
+					vim.opt_local.tabstop = 4
+					vim.opt_local.shiftwidth = 4
+					vim.opt_local.softtabstop = 4
+				end,
+			})
+		end,
+		opts = {
+			servers = {
+				fsautocomplete = {
+					root_markers = { ".sln", ".fsproj", ".fsx", ".slnx" },
+				},
+			},
+		},
+	},
 
-  -- Install F# treesitter parser alongside the base parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "fsharp" })
-      return opts
-    end,
-  },
+	-- Install F# treesitter parser alongside the base parsers
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "fsharp" })
+			return opts
+		end,
+	},
 
-  -- Conform (unchanged — LSP handles formatting)
-  {
-    "stevearc/conform.nvim",
-    ft = { "fsharp" },
-    opts = function(_, opts)
-      opts.formatters = opts.formatters or {}
-      opts.formatters.fantomas = {
-        command = "dotnet",
-        args = { "fantomas", "$FILENAME" },
-        stdin = false,
-      }
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.fsharp = { "fantomas" }
-    end,
-  },
+	-- Conform (unchanged — LSP handles formatting)
+	{
+		"stevearc/conform.nvim",
+		ft = { "fsharp" },
+		opts = function(_, opts)
+			opts.formatters = opts.formatters or {}
+			opts.formatters.fantomas = {
+				command = "dotnet",
+				args = { "fantomas", "$FILENAME" },
+				stdin = false,
+			}
+			opts.formatters_by_ft = opts.formatters_by_ft or {}
+			opts.formatters_by_ft.fsharp = { "fantomas" }
+		end,
+	},
 
-  -- DAP
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "netcoredbg" })
-      return opts
-    end,
-  },
+	-- DAP
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "netcoredbg" })
+			return opts
+		end,
+	},
 }
