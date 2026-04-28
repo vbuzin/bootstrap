f28a73871d1e3ab5101410ef50c9f76102bad4a1
Slava Buzin <31203090+vbuzin@users.noreply.github.com>
2026-04-28 21:04:37 +0200

    zed config update

--- nvim/nvim/lua/plugins/coding.lua
+++ nvim/nvim/lua/plugins/coding.lua
@@ -190,16 +190,6 @@ return {
 				callback = function(event)
 					local bufnr = event.buf
 
-					-- Enable inlay hints for F# if supported
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if
-						client
-						and client.name == "fsautocomplete"
-						and client:supports_method("textDocument/inlayHint")
-					then
-						vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-					end
-
 					local function map(mode, lhs, rhs, desc)
 						vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, buffer = bufnr, desc = desc })
 					end
